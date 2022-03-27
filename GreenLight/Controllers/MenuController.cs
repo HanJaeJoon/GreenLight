064ed57a5d9fe65ae3c04b5b58d62e4a788f1f43
jaejoon.han <jaejoon.han@crevisse.com>
2022-03-28 01:36:11 +0900

    init

---
+++ GreenLight/Controllers/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using GreenLight.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenLight.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MenuController : Controller
+{
+    private readonly MenuService _menuService;
+
+    public MenuController()
+    {
+        _menuService = new MenuService();
+    }
+
+    //[HttpGet]
+    //public ActionResult GetRecentMenu()
+    //{
+    //    return _menuService.GetRecentMenu();
+    //}
+
+    [HttpGet("{date?}")]
+    public async Task<ActionResult<string>> GetAsync(DateTime date)
+    {
+        string? menu = await _menuService.GetAsync(date);
+
+        if (menu == null)
+        {
+            return NotFound();
+        }
+
+        return menu;
+    }
+}
---
+++ GreenLight/Services/MenuService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace GreenLight.Services;
+
+public class MenuService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _instagramId = "green_food_buffet";
+
+    public MenuService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    //public List<string> GetRecentMenu() 
+    //{
+    //    return new List<string>();
+    //}
+
+    public async Task<string?> GetAsync(DateTime date)
+    {
+        string url = $"https://www.instagram.com/{_instagramId}/?a=1";
+
+        HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+        if (response is null)
+        {
+            return null;
+        }
+
+        response.EnsureSuccessStatusCode();
+        string result = await response.Content.ReadAsStringAsync();
+
+        JsonSerializer.Deserialize<RootObject1>(result);
+
+        return result;
+    }
+}
