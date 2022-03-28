beea1ffb6d0020497dce82bf52a7759522280d5f
jaejoon.han <jaejoon.han@crevisse.com>
2022-03-28 22:45:31 +0900

    dddddd

--- GreenLight/Services/MenuService.cs
+++ GreenLight/Services/MenuService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace GreenLight.Services;
 
@@ -21,17 +22,34 @@ public class MenuService
     {
         string url = $"https://www.instagram.com/{_instagramId}/?a=1";
 
-        HttpResponseMessage response = await _httpClient.GetAsync(url);
+        //_httpClient.DefaultRequestHeaders.Add("Accept",
+        //    "application/json");
 
-        if (response is null)
+        //HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+        //if (response is null)
+        //{
+        //    return null;
+        //}
+
+        //response.EnsureSuccessStatusCode();
+        //string result = await response.Content.ReadAsStringAsync();
+
+        string result = string.Empty;
+
+        using (IWebDriver driver = new ChromeDriver())
         {
-            return null;
+            driver.Url = url;
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var elements = driver.FindElements(By.CssSelector("body"));
+
+            foreach (var el in elements)
+            {
+                result += el.FindElement(By.CssSelector("pre")).Text.Trim();
+            }
         }
-
-        response.EnsureSuccessStatusCode();
-        string result = await response.Content.ReadAsStringAsync();
-
-        JsonSerializer.Deserialize<RootObject1>(result);
 
         return result;
     }
