5c4cb9dd499df52a6530cc46f002eedc53b25614
jaejoon.han <jaejoon.han@crevisse.com>
2022-04-03 17:08:33 +0900

    Revert "전체 document 저장"
    This reverts commit 08c78b61ec67b6db2f56da0893b69c59b2e72d35.

--- GreenLight/Services/MenuService.cs
+++ GreenLight/Services/MenuService.cs
@@ -49,7 +49,7 @@ public class MenuService
         };
     }
 
-    private async Task<string> GetInstagramDataAsync(string url)
+    private static async Task<string> GetInstagramDataAsync(string url)
     {
         string result = string.Empty;
 
@@ -60,10 +60,6 @@ public class MenuService
             var document = await context.OpenAsync(url);
 
             await document.WaitForReadyAsync();
-
-            string path = Path.Combine(_env.WebRootPath, "data");
-
-            _ = File.WriteAllTextAsync(Path.Combine(path, $"raw.json"), document.ToHtml());
 
             var preList = document.QuerySelectorAll("pre");
 
